1c5fd22573e8a1d3ddf89ff7528a06500e7cbe62
LeooDonaa <leonardo.dona08@mail.com>
2023-05-02 09:34:49 -0300

    banco

--- Banco/banco.sql
+++ Banco/banco.sql
@@ -1,1 +1,14 @@
-mysql -u root
+DROP DATABASE IF EXISTS projeto1;
+
+CREATE DATABASE projeto1;
+USE projeto1;
+
+CREATE TABLE usuario
+(
+	id_usuario int(4) PRIMARY KEY auto_increment,
+    nome_usuario varchar(50) not null,
+    email_usuario varchar(50) not null,
+    fone_usuario varchar(30) null,
+    senha varchar(30) not null
+    );
+
