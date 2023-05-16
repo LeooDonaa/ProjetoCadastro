d369fca27daa580484551d9eafe1323c7d373cb0
LeooDonaa <leonardo.dona08@mail.com>
2023-05-16 08:49:46 -0300

    Joana, a fada do desgosto.

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -8,9 +8,20 @@ $fone = $_POST['fone'];
 $senha = $_POST['senha'];
 
 echo "<h1>Dados do usuário</h1>";
-echo "Nome: $nome<br>";
-echo "Email: $email<br>";
-echo "Telefone: $fone<br>";
-echo "Senha: $senha<br>";
+echo "Nome: $nome <br>";
+echo "Email: $email <br>";
+echo "Telefone: $fone <br>";
+echo "Senha: $senha <br>";
+
+$sql = "INSERT INTO usuario (nome_usuario, email_usuario,
+                            fone_usuario, senha)";
+$sql .= "VALUES ('".$nome."','".$email."','".
+                    $fone."','".$senha."')";
+echo $sql."<br>";
+$result = mysqli_query($con, $sql);
+if($result)
+    echo "Dados cadastrados com sucesso!";
+    else
+        echo "Erro ao tentar cadastrar!";
 
 ?>
